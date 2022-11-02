116dd352bf4924c59cff5cfa73bc4244929aee55
Sander Molenkamp <a.molenkamp@gmail.com>
2022-11-02 14:40:31 +0100

    WIP

--- src/VehicleRegistrationService/Controllers/VehicleInfoController.cs
+++ src/VehicleRegistrationService/Controllers/VehicleInfoController.cs
@@ -7,6 +7,8 @@ public class VehicleInfoController : ControllerBase
     private readonly ILogger<VehicleInfoController> _logger;
     private readonly IVehicleInfoRepository _vehicleInfoRepository;
 
+    private static int _callCount;
+
     public VehicleInfoController(ILogger<VehicleInfoController> logger, IVehicleInfoRepository vehicleInfoRepository)
     {
         _logger = logger;
@@ -14,9 +16,20 @@ public class VehicleInfoController : ControllerBase
     }
 
     [HttpGet("{licenseNumber}")]
-    public ActionResult<VehicleInfo> GetVehicleInfo(string licenseNumber)
+    public async Task<ActionResult> GetVehicleInfo(string licenseNumber)
     {
+        _callCount += 1;
+
         _logger.LogInformation($"Retrieving vehicle-info for licensenumber {licenseNumber}");
+
+        if (_callCount % 3 != 0)
+        {
+            return base
+        }
+
+        // Take some time to trigger resiliency timeout.
+        await Task.Delay(TimeSpan.FromSeconds(3));
+
         VehicleInfo info = _vehicleInfoRepository.GetVehicleInfo(licenseNumber);
         return info;
     }
